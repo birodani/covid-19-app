5dd02556d760dc6b478a72c003abdeb10b120d06
birodani <birodani91@gmail.com>
2020-08-17 10:50:03 +0200

    add materials, imporve components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './App.css';
 import { Cards, CountryPicker, Chart } from './components';
+import { fetchDailyData } from './api/network.connector';
+import style from './App.module.css'
 
 class App extends React.Component {
+
+  async componentDidMount() {
+    const data = await fetchDailyData();
+    console.log(data);
+  }
   render() {
     return (
-      <div>
+      <div className={style.container}>
         <Cards></Cards>
         <CountryPicker></CountryPicker>
         <Chart></Chart>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import styles from './Card.module.css';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Grid from '@material-ui/core/Grid'
 
 const CardComponent = () => (
-  <div>CardComponent</div>
+    <Grid container spacing={3}>
+        <Grid item>
+            <Card>
+                <CardContent>
+                    fdfds
+                </CardContent>
+            </Card>
+        </Grid>
+        <Grid item>
+            <Card>
+                <CardContent>
+                    fdfds
+                </CardContent>
+            </Card>
+        </Grid>
+        <Grid item>
+            <Card>
+                <CardContent>
+                    fdfds
+                </CardContent>
+            </Card>
+        </Grid>
+    </Grid>
 );
 
 export default CardComponent;
